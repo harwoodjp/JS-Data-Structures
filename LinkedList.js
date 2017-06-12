80deee97e581f6f9f037b360a20ba32f6da19c32
Justin Harwood <harwoodjp@g.cofc.edu>
2017-06-12 15:58:31 -0400

    return/del node by value

--- LinkedList.js
+++ LinkedList.js
@@ -9,15 +9,8 @@ class LinkedList {
 	constructor() {
 		this.head = null;
 	}
-	setHead(head) {
-		this.head = head;
-	}
-	printList() {
-		let tmpHead = this.head;
-		while(tmpHead) {
-			console.log(tmpHead)
-			tmpHead = tmpHead.link;
-		}
+	get length() {
+		return this.getLength();
 	}
 	getLength() {
 		let tmpHead = this.head;
@@ -28,15 +21,58 @@ class LinkedList {
 		}
 		return length;
 	}
+	getNodeByValue(value) {
+		let tmpHead = this.head;
+		while(tmpHead) {
+			if (tmpHead.value === value) {
+				return tmpHead
+			}
+			tmpHead = tmpHead.link;
+		}
+	}
+	printList(readable) {
+		let tmpHead = this.head;
+		while(tmpHead) {
+			readable ? console.log(`Node(${tmpHead.value})`) : console.log(tmpHead)			
+			tmpHead = tmpHead.link;
+		}
+	}	
+	removeNodeByValue(value) {
+		let tmpHead = this.head;
+		while(tmpHead) {
+			if (tmpHead.value === value) {
+				if (tmpHead.link) {
+					this.head = tmpHead.link;
+				}
+				else {
+					this.head = null;
+				}
+			}
+
+			if (tmpHead.link && (tmpHead.link.value === value)) {
+				if (tmpHead.link.link) {
+					tmpHead.link = tmpHead.link.link
+				} else {
+					tmpHead.link = null;
+				}
+			}
+			tmpHead = tmpHead.link;
+		}
+	}
 }
 
-
-const node3 = new Node(3, null),
-	node2 = new Node(2, node3),
-	node1 = new Node(1, node2),
-	node0 = new Node(0, node1);
+const node3 = new Node("end", null),
+	node2 = new Node(false, node3),
+	node1 = new Node(4354, node2),
+	node0 = new Node("first/head", node1);
 
 let list = new LinkedList();
-list.setHead(node0);
-list.printList();
-//list.getLength();
+
+list.head = node0;
+
+list.printList(1);
+list.removeNodeByValue("end");
+console.log("\n");
+list.printList(1);
+
+
